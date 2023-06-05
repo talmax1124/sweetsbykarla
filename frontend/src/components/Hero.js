3a97684ed9e54f85a1ddfef40ee5a2f0ef133518
talmax1124 <carlitosdiazplaza@gmail.com>
2023-06-04 21:47:05 -0400

    Update

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -3,49 +3,45 @@
 /* eslint-disable jsx-a11y/iframe-has-title */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import CarouselImage from "../assets/work.gif";
+
 const Hero = () => {
   return (
     <>
       <div className="-mt-4">
-        <div className="hero">
-          <div className="hero-intro">
-            <div className="hero-intro-content">
-              <h1 className="hero-intro-title">
-                <span className="hero-intro-title-1 text-black font-bold">
-                  Freshly Baked
-                </span>
-                <br />
-                <span className="hero-intro-title-2 font-medium text-slate-600">
-                  Everyday
-                </span>
-              </h1>
-              <p>
-                From the hand of a professional baker, we provide you the
-                high-quality sweets for your sweet tooth.
-              </p>
+        <div className="hero-alternative">
+          <div className="hero-alternative-intro">
+            <h1 className="hero-alternative-intro-title-1 text-black font-bold">
+              Sweet Bakery
+            </h1>
+            <h1 className="hero-alternative-intro-title-2 font-extrabold text-[#F1646C]">
+              Delights Await<span className="font-bold">.</span>
+            </h1>
+
+            <p className="mb-3">
+              Get ready to Experience a delectable journey where heavenly
+              flavors and irresistible aromas dance together, indulging your
+              senses in a symphony of sweetness.
+            </p>
+            <div>
+              <Link
+                to="/products"
+                className="btn bg-[#7e4f55] text-white w-1/4 hover:bg-[#A06C74] rounded"
+              >
+                <i className="fas fa-shopping-bag"></i> Shop Now
+              </Link>
+              <Link
+                to="/contact"
+                className="btn contact-button rounded w-1/5 ml-2"
+              >
+                <i className="fas fa-envelope"></i> Contact Us
+              </Link>
             </div>
           </div>
-          <div className="product-info-carousel">
-            <div className="product-image-big"></div>
-            <div className="product-step-carousel">
-              <div className="step-image"></div>
-              <div className="step-control">
-                <span>
-                  <i className="fas fa-chevron-left control-icon-1"></i>
-                </span>
-                <span>
-                  <i className="fas fa-chevron-right control-icon-2"></i>
-                </span>
-              </div>
-              <div className="step-name">
-                <div className="flex justify-center mt-auto mb-auto items-center flex-col">
-                  <h1>Red Velvet Cake</h1>
-                  <p>$ 36.99</p>
-                </div>
-                <button>Add To Cart</button>
-              </div>
-            </div>
+          {/* GIF */}
+          <div className="products-gif">
+            <img src={CarouselImage} alt="hero" />
           </div>
         </div>
       </div>
